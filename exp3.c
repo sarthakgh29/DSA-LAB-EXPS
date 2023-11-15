4512fe97fac70d985f2cd3d6c2548624050f7cc2
Sarthak Sharma <142618314+sarthakgh29@users.noreply.github.com>
2023-11-15 21:00:40 +0530

    Create exp3.c

---
+++ exp3.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to allocate memory for a matrix
+int** allocateMatrix(int rows, int cols) {
+    int** matrix = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (int*)malloc(cols * sizeof(int));
+    }
+    return matrix;
+}
+
+// Function to free memory allocated for a matrix
+void freeMatrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+// Function to perform matrix multiplication
+int** matrixMultiply(int** matrix1, int** matrix2, int rows1, int cols1, int cols2) {
+    int** result = allocateMatrix(rows1, cols2);
+    
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    
+    return result;
+}
+
+// Function to display a matrix
+void displayMatrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows1, cols1, rows2, cols2;
+
+    printf("Enter the number of rows and columns for the first matrix: ");
+    scanf("%d %d", &rows1, &cols1);
+    
+    printf("Enter the number of rows and columns for the second matrix: ");
+    scanf("%d %d", &rows2, &cols2);
+
+    if (cols1 != rows2) {
+        printf("Matrix multiplication is not possible. The number of columns in the first matrix must be equal to the number of rows in the second matrix.\n");
+        return 1;
+    }
+
+    int** matrix1 = allocateMatrix(rows1, cols1);
+    int** matrix2 = allocateMatrix(rows2, cols2);
+
+    printf("Enter elements for the first matrix:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter elements for the second matrix:\n");
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    int** result = matrixMultiply(matrix1, matrix2, rows1, cols1, cols2);
+
+    printf("Resultant matrix:\n");
+    displayMatrix(result, rows1, cols2);
+
+    // Free allocated memory
+    freeMatrix(matrix1, rows1);
+    freeMatrix(matrix2, rows2);
+    freeMatrix(result, rows1);
+
+    return 0;
+}
